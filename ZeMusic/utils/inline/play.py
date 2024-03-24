31cb4dcf171a8b423a6b4638cb48e467664d55f6
Abdullah <62890648+king-1-9@users.noreply.github.com>
2024-03-25 02:10:59 +0300

    Update play.py

--- ZeMusic/utils/inline/play.py
+++ ZeMusic/utils/inline/play.py
@@ -61,13 +61,12 @@ def stream_markup_timer(_, chat_id, played, dur):
                 callback_data="GetTimer",
             )
         ],[
-            InlineKeyboardButton(text="إستئناف ⌯", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="إيقاف مؤقت", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="⌯ إعاده", callback_data=f"ADMIN Replay|{chat_id}"),
-        ],[
-            InlineKeyboardButton(text="⌯ تخطي ⌯", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="⌯ توقف ⌯", callback_data=f"ADMIN Stop|{chat_id}"),
-        ],[
+            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
+            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+         ],[
             InlineKeyboardButton(text="⌯ اضف البوت الى مجموعتك او قناتك ⌯", url=f"https://t.me/{app.username}?startgroup=true")],
     ]
     return buttons
