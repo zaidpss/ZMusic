68057118d906b58656ab366ca2fc7a7bfd45c9e4
Abdullah <62890648+king-1-9@users.noreply.github.com>
2024-03-28 18:15:24 +0300

    Update mongo.py

--- ZeMusic/core/mongo.py
+++ ZeMusic/core/mongo.py
@@ -4,11 +4,11 @@ from config import MONGO_DB_URI
 
 from ..logging import LOGGER
 
-LOGGER(__name__).info("Connecting to your Mongo Database...")
+LOGGER(__name__).info("يتم رفع البيانات...")
 try:
     _mongo_async_ = AsyncIOMotorClient(MONGO_DB_URI)
     mongodb = _mongo_async_.Elhyba
-    LOGGER(__name__).info("Connected to your Mongo Database.")
+    LOGGER(__name__).info("تم رفع البيانات.")
 except:
-    LOGGER(__name__).error("Failed to connect to your Mongo Database.")
+    LOGGER(__name__).error("خطاء.  لم يتم رفع البيانات")
     exit()
