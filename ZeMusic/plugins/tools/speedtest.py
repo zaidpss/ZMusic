6b42d07d5d405c989c25f242aec2c32f9794e36d
Abdullah <62890648+king-1-9@users.noreply.github.com>
2024-04-13 03:41:00 +0300

    Update speedtest.py

--- ZeMusic/plugins/tools/speedtest.py
+++ ZeMusic/plugins/tools/speedtest.py
@@ -25,7 +25,7 @@ def testspeed(m, _):
     return result
 
 
-@app.on_message(filters.command(["speedtest", "السرعه"]) & SUDOERS)
+@app.on_message(filters.command(["speedtest", "اختبار السرعه"]) & SUDOERS)
 @language
 async def speedtest_function(client, message: Message, _):
     m = await message.reply_text(_["server_11"])
