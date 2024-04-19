8837702d8c9fd89a069c4d5cbd65bd5eeb16375e
Abdullah <62890648+king-1-9@users.noreply.github.com>
2024-04-19 19:56:12 +0300

    Update download.py

--- ZeMusic/plugins/play/download.py
+++ ZeMusic/plugins/play/download.py
@@ -19,7 +19,7 @@ def remove_if_exists(path):
         os.remove(path)
 
 
-@app.on_message(command(["song","/song", "تنزيل", "تحميل","بحث"]))
+@app.on_message(command(["song","/song", "بحث"]))
 async def song_downloader(client, message: Message):
     query = " ".join(message.command[1:])
     m = await message.reply_text("<b>⇜ جـارِ البحث عـن المقطـع الصـوتـي . . .</b>")
