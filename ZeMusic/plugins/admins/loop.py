a33d629cbc2cacc91bf6938deebc6077856e3b52
Abdullah <62890648+king-1-9@users.noreply.github.com>
2024-04-24 17:02:35 +0300

    Update loop.py

--- ZeMusic/plugins/admins/loop.py
+++ ZeMusic/plugins/admins/loop.py
@@ -8,7 +8,7 @@ from ZeMusic.utils.inline import close_markup
 from config import BANNED_USERS
 
 
-@app.on_message(filters.command(["loop", "cloop", "تكرار", "التكرار"]) & filters.group & ~BANNED_USERS)
+@app.on_message(filters.command(["/loop", "/cloop", "تكرار", "التكرار"],"") & filters.group & ~BANNED_USERS)
 @AdminRightsCheck
 async def admins(cli, message: Message, _, chat_id):
     usage = _["admin_17"]
