876debb288e9bddd69cc73034351ad70637f75fd
Abdullah <62890648+king-1-9@users.noreply.github.com>
2024-05-01 12:06:16 +0300

    Update devm.py

--- ZeMusic/plugins/play/devm.py
+++ ZeMusic/plugins/play/devm.py
@@ -20,7 +20,7 @@ async def devid(client: Client, message: Message):
        
     await message.reply_photo(
         photo="downloads/developer.jpg",
-        caption=f"""<b>⌯ 𝙽𝙰𝙼𝙴 :</b> <a href='tg://user?id={uid}'>{name}</a>\n\n<b>⌯ 𝚄𝚂𝙴𝚁𝙽𝙰𝙼𝙴 :</b> @{usrnam}""",
+        caption=f"""<b>⌯ 𝙽𝙰𝙼𝙴 :</b> <a href='tg://user?id={uid}'>{name}</a>\n\n<b>⌯ 𝚄𝚂𝙴𝚁 :</b> @{usrnam}""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
