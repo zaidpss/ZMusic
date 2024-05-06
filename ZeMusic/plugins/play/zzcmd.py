f600c04f68d8be8239ee3800f5205978497a9b24
Abdullah <62890648+king-1-9@users.noreply.github.com>
2024-05-06 13:42:40 +0300

    Update zzcmd.py

--- ZeMusic/plugins/play/zzcmd.py
+++ ZeMusic/plugins/play/zzcmd.py
@@ -28,7 +28,7 @@ async def zzzback(_, query: CallbackQuery):
                         "• اوامــر المطــور •", callback_data="zzzdv"),
                 ],[
                     InlineKeyboardButton(
-                        text=config.CHANNEL_NAME, url=config.CHANNEL_LINK),
+                        text=config.CHANNEL_NAME, url=lnk),
                 ],
             ]
         ),
